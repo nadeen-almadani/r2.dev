90eaf17cd14b17bfedcfbd58822d359193fb4b70
nadeen-almadani <nadeenalma92@gmail.com>
2023-01-24 22:49:23 +0200

    bbbbbbbbbbbbb

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@
 function App() {
   return (
     <>
-       <h1>tala julye</h1>
-      <h1 className="title">nadeen</h1>
+       <h1>tala julye ali bek</h1>
+      <h1 className="title">nadeen almadani</h1>
       <h2>ahmad for ever</h2>
       <h2>ali bek</h2>
     </>
